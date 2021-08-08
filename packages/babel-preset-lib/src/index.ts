8ea1c639dc34c1e6e3aafd488b088c2767abf18c
tangbohao37 <914440302@qq.com>
2021-08-08 17:29:01 +0800

    feat(): test

--- packages/babel-preset-lib/src/index.ts
+++ packages/babel-preset-lib/src/index.ts
@@ -37,7 +37,7 @@ export default declare((api, options) => {
           libraryDirectory: 'es', //
           style: true,
         },
-        'antd', // 对antd 使用
+        'antd-mobile', // 对antd 使用
       ],
     ],
   } as any
